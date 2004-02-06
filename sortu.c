d8fe058338b0887ad899704d40d503870aaf4b1d
gray <>
2004-02-06 00:40:40 +0000

    added --order-sort and start/stop line offset

--- sortu.c
+++ sortu.c
@@ -6,7 +6,9 @@
  * $Id$
  */
 
+#include <ctype.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "argv.h"
@@ -15,9 +17,11 @@
 #define DEFAULT_DELIM	" "
 #define LINE_SIZE	1024
 
-/*
- * NOTE: RANLIB_PROG and STRIP_PROG should be defined in Buildfile
- */
+/* struct for the order/count stuff */
+typedef struct {
+  unsigned long	so_count;			/* count that we have seen the item */
+  int		so_order;			/* order that we get it for -o */
+} sortu_t;
 
 /* argument variables */
 static	int		ignore_blanks_b = 0;	/* ignore blank lines */
@@ -31,9 +35,12 @@ static	int		loose_fields_b = 0;	/* loose field match */
 static	int		min_matches = 0;	/* minimum number of matches */
 static	int		max_matches = 0;	/* max number of matches */
 static	int		numbers_b = 0;		/* fields are numbers */
-static	int		float_numbers_b = 0;	/* fields are floats */
+static	int		numbers_float_b = 0;	/* fields are floats */
+static	int		order_sort_b = 0;	/* keep order when sorting */
 static	int		show_percentage_b = 0;	/* show percentage vals */
 static	int		reverse_sort_b = 0;	/* reverse the sort order */
+static	int		start_offset = 0;	/* field starts at this offset */
+static	int		stop_offset = 0;	/* field stops at this offset */
 static	int		verbose_b = 0;		/* verbose flag */
 static	argv_array_t	files;			/* work files */
 
@@ -62,12 +69,18 @@ static	argv_t	args[] = {
   { ARGV_OR },
   { 'n',	"numbers",	ARGV_BOOL_INT,		&numbers_b,
     NULL,		"treat field as signed long number" },
-  { 'N',	"float-numbers", ARGV_BOOL_INT,		&float_numbers_b,
+  { 'N',	"float-numbers", ARGV_BOOL_INT,		&numbers_float_b,
     NULL,		"treat field as floating point" },
+  { 'o',	"order-sort",	ARGV_BOOL_INT,		&order_sort_b,
+    NULL,		"output in order of discovery" },
   { 'p',	"percentage-show", ARGV_BOOL_INT,	&show_percentage_b,
     NULL,		"show percentage along with count" },
   { 'r',	"reverse-sort",	ARGV_BOOL_INT,		&reverse_sort_b,
     NULL,		"reverse the sort" },
+  { 's',	"start-offset",	ARGV_INT,		&start_offset,
+    "offset",		"field starts at offet" },
+  { 'S',	"stop-offset",	ARGV_INT,		&stop_offset,
+    "offset",		"field stops at offset" },
   { 'v',	"verbose",	ARGV_BOOL_INT,		&verbose_b,
     NULL,		"verbose mode" },
   { ARGV_MAYBE,	NULL,		ARGV_CHAR_P | ARGV_FLAG_ARRAY, &files,
@@ -109,20 +122,24 @@ static	int	count_compare(const void *key1_p, const int key1_size,
 			      const void *key2_p, const int key2_size,
 			      const void *data2_p, const int data2_size)
 {
+  const sortu_t	*sortu1_p = data1_p, *sortu2_p = data2_p;
   const double	*double1_p, *double2_p;
   const long	*long1_p, *long2_p;
   const char	*str1_p, *str2_p;
   int		result;
   
+  if (order_sort_b) {
+    /* the order will always be uniq */
+    return sortu1_p->so_order - sortu2_p->so_order;
+  }
+  
   /* if we aren't sorting by key then sort the count */
   if (! key_sort_b) {
-    long1_p = data1_p;
-    long2_p = data2_p;
     if (reverse_sort_b) {
-      result = *long2_p - *long1_p;
+      result = sortu2_p->so_count - sortu1_p->so_count;
     }
     else {
-      result = *long1_p - *long2_p;
+      result = sortu1_p->so_count - sortu2_p->so_count;
     }
     /* if the count is == then sort by key */
     if (result != 0) {
@@ -141,7 +158,7 @@ static	int	count_compare(const void *key1_p, const int key1_size,
       return *long1_p - *long2_p;
     }
   }
-  else if (float_numbers_b) {
+  else if (numbers_float_b) {
     /* reverse numeric sort */
     double1_p = key1_p;
     double2_p = key2_p;
@@ -168,13 +185,14 @@ static	int	count_compare(const void *key1_p, const int key1_size,
 int	main(int argc, char **argv)
 {
   FILE		*infile;
-  char		*filename, line[LINE_SIZE], *tok, *line_p;
+  char		*filename, line[LINE_SIZE], *tok, *line_p, *line_bounds_p;
   int		file_c, ret, field_c, key_size, entry_n;
-  unsigned long	count, *count_p, total, subtotal, val, perc;
+  unsigned long	total, subtotal, perc;
   long		value;
   double	double_value;
   void		*key_p;
   table_t	*tab;
+  sortu_t	sortu, *sortu_p;
   table_entry_t	**entries, **entries_p;
   
   argv_process(args, argc, argv);
@@ -208,9 +226,14 @@ int	main(int argc, char **argv)
     exit(1);
   }
   
+  /* initialize our sortu insert structure */
+  sortu.so_count = 1;
+  sortu.so_order = 0;
+  
   file_c = 0;
   while (1) {
     
+    /* process each of the files */
     if (file_c == 0 && ARGV_ARRAY_COUNT(files) == 0) {
       filename = "stdin";
       infile = stdin;
@@ -225,10 +248,26 @@ int	main(int argc, char **argv)
       }
     }
     
+    if (numbers_b) {
+      key_p = &value;
+      key_size = sizeof(value);
+    }
+    else if (numbers_float_b) {
+      key_p = &double_value;
+      key_size = sizeof(double_value);
+    }
+    
     while (fgets(line, sizeof(line), infile) != NULL) {
-      for (line_p = line; *line_p != '\n' && *line_p != '\0'; line_p++) {
+      
+      /* cut off the \n */
+      for (line_bounds_p = line;
+	   *line_bounds_p != '\n' && *line_bounds_p != '\0';
+	   line_bounds_p++) {
       }
-      *line_p = '\0';
+      if (stop_offset > 0 && line_bounds_p > line + stop_offset) {
+	line_bounds_p = line + stop_offset;
+      }
+      *line_bounds_p = '\0';
       
       /* if blank line then maybe ignore it */
       if (line[0] == '\0' && ignore_blanks_b) {
@@ -239,6 +278,8 @@ int	main(int argc, char **argv)
       tok = line;
       line_p = line;
       for (field_c = field - 1; field_c >= 0;) {
+	
+	/* find the correct field in the line if necessary */ 
 	tok = strsep(&line_p, delim_str);
 	if (tok == NULL) {
 	  break;
@@ -259,36 +300,40 @@ int	main(int argc, char **argv)
       
       if (numbers_b) {
 	value = atol(tok);
-	key_p = &value;
-	key_size = sizeof(value);
       }
-      else if (float_numbers_b) {
+      else if (numbers_float_b) {
 	double_value = atof(tok);
-	key_p = &double_value;
-	key_size = sizeof(double_value);
       }
       else {
+	key_p = tok + start_offset;
 	if (case_insens_b) {
 	  /* lower case the string */
-	  for (line_p = tok; *line_p != '\0'; line_p++) {
+	  for (; line_p < line_bounds_p; line_p++) {
 	    *line_p = tolower(*line_p);
 	  }
 	}
-	key_p = tok;
-	key_size = -1;
+	key_size = line_bounds_p - (char *)key_p;
+	/* check to make sure we have a field */
+	if (key_size <= 0) {
+	  continue;
+	}
       }
       
-      count = 1;
-      ret = table_insert(tab, key_p, key_size, &count, sizeof(count),
-			 (void **)&count_p, 0);
-      if (ret != TABLE_ERROR_NONE) {
+      /* add it into the table */
+      ret = table_insert(tab, key_p, key_size, &sortu, sizeof(sortu),
+			 (void **)&sortu_p, 0);
+      if (ret == TABLE_ERROR_NONE) {
+	sortu.so_order++;
+      }
+      else {
 	if (ret != TABLE_ERROR_OVERWRITE) {
 	  (void)fprintf(stderr, "%s: could not add key to table: %s\n",
 			argv_program, table_strerror(ret));
 	  exit(1);
 	}
 	
-	(*count_p)++;
+	/* it exists already so add one to the count */
+	sortu_p->so_count++;
       }
     }
     
@@ -314,7 +359,7 @@ int	main(int argc, char **argv)
 	(void)printf(" %5.5s", "C %:");
       }
     }
-    if (numbers_b || float_numbers_b) {
+    if (numbers_b || numbers_float_b) {
       (void)printf(" %10.10s\n", "Data:");
     }
     else {
@@ -351,7 +396,7 @@ int	main(int argc, char **argv)
   for (entries_p = entries; entries_p < entries + entry_n; entries_p++) {
     /* get each entry to print */
     ret = table_entry(tab, *entries_p, (void **)&key_p, &key_size,
-		      (void **)&count_p, NULL);
+		      (void **)&sortu_p, NULL);
     if (ret != TABLE_ERROR_NONE) {
       (void)fprintf(stderr, "%s: could not get table entry: %s\n",
 		    argv_program, table_strerror(ret));
@@ -359,19 +404,19 @@ int	main(int argc, char **argv)
     }
     
     /* limit the matches if necessary */
-    if (*count_p < min_matches
-	|| (max_matches > 0 && *count_p > max_matches)) {
+    if (sortu_p->so_count < min_matches
+	|| (max_matches > 0 && sortu_p->so_count > max_matches)) {
       continue;
     }
     
-    total += *count_p;
+    total += sortu_p->so_count;
   }
   
   subtotal = 0;
   for (entries_p = entries; entries_p < entries + entry_n; entries_p++) {
     /* get each entry to print */
     ret = table_entry(tab, *entries_p, (void **)&key_p, &key_size,
-		      (void **)&count_p, NULL);
+		      (void **)&sortu_p, NULL);
     if (ret != TABLE_ERROR_NONE) {
       (void)fprintf(stderr, "%s: could not get table entry: %s\n",
 		    argv_program, table_strerror(ret));
@@ -379,15 +424,15 @@ int	main(int argc, char **argv)
     }
     
     /* limit the matches if necessary */
-    if (*count_p < min_matches
-	|| (max_matches > 0 && *count_p > max_matches)) {
+    if (sortu_p->so_count < min_matches
+	|| (max_matches > 0 && sortu_p->so_count > max_matches)) {
       continue;
     }
     
-    subtotal += *count_p;
+    subtotal += sortu_p->so_count;
     
     if (! no_counts_b) {
-      (void)printf("%10lu ", *count_p);
+      (void)printf("%10lu ", sortu_p->so_count);
       
       if (cumulative_b) {
 	(void)printf("%10lu ", subtotal);
@@ -396,10 +441,10 @@ int	main(int argc, char **argv)
     
     if (show_percentage_b) {
       if (total > 1000000) {
-	perc = *count_p / (total / 100);
+	perc = sortu_p->so_count / (total / 100);
       }
       else {
-	perc = *count_p * 100 / total;
+	perc = sortu_p->so_count * 100 / total;
       }
       (void)printf("%4ld%% ", perc);
       
@@ -417,7 +462,7 @@ int	main(int argc, char **argv)
     if (numbers_b) {
       (void)printf("%10ld\n", *(long *)key_p);
     }
-    else if (float_numbers_b) {
+    else if (numbers_float_b) {
       (void)printf("%10.2f\n", *(double *)key_p);
     }
     else {
@@ -447,7 +492,7 @@ int	main(int argc, char **argv)
 	(void)printf("%5.5s ", "100%");
       }
     }
-    if (numbers_b || float_numbers_b) {
+    if (numbers_b || numbers_float_b) {
       (void)printf("%10.10s\n", "Total");
     }
     else {
